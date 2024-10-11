0b92d1b3dc89366fb4c911dd5a5934cb0e0be095
baileyluTCD <baileylu@tcd.ie>
2024-10-11 19:49:24 +0100

    added counter

--- src/pages/content/components/Demo/app.tsx
+++ src/pages/content/components/Demo/app.tsx
@@ -1,8 +1,11 @@
 import logo from "@src/assets/img/logo.svg";
 import "@src/styles/index.css";
+import { createSignal } from 'solid-js';
 import styles from "./App.module.css";
 
 const App = () => {
+  const [count, setCount] = createSignal(0);
+
   return (
     <div class="fixed right-5 top-20 z-[2000] w-80 rounded-xl bg-white">
       <div class={styles.App}>
@@ -14,7 +17,12 @@ const App = () => {
           />
           <p class="flex flex-wrap font-bold">
             Edit <code>src/pages/content/index.tsx</code> and save to reload.
+
+            Count: {count()}
           </p>
+          <button on:click={() => setCount(count() + 1)}>
+            Click me
+          </button>
           <a
             href="https://github.com/solidjs/solid"
             target="_blank"
