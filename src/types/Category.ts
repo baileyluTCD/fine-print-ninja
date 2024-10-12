0aa7b41c6648f5828e41506564c8751205644ce9
baileyluTCD <baileylu@tcd.ie>
2024-10-12 19:31:29 +0100

    partial conversion of tensorflow code

---
+++ src/types/Category.ts
@@ -0,0 +1,13 @@
+export enum Category {
+  Tracking = "Tracking",
+  DataCollection = "Data Collection",
+  ThirdPartySharing = "Third-Party Sharing",
+  Surveillance = "Surveillance",
+  UserContentAndRights = "UserContentAndRights",
+  DataRetention = "Data Retention",
+  SecurityMeasures = "Security Measures",
+  Cookies = "Cookies and Similar Technologies",
+  DataTransfers = "Data Transfers and Storage Location",
+  UserProfiling = "User Profiling and Automated Decision Making",
+  DataBreachNotification = "Data Breach Notification",
+}
