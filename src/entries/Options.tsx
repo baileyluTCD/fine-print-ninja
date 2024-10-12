4cc933529a858ae61609538a8b174ba827144cb3
josephreidy <161133156+josephreidy@users.noreply.github.com>
2024-10-12 12:08:35 +0100

    removed flexbox to see if conflicting with margin sizes

--- src/entries/Options.tsx
+++ src/entries/Options.tsx
@@ -6,7 +6,7 @@ export function Options() {
   const [detectPopup, setDetectPopup] = createSignal(true);
 
   return (
-    <main class="flex flex-col items-center justify-center h-screen">
+    <main>
       <h1 class="text-2xl font-bold mb-12">Fine Print Ninja Options</h1>
       <p>Enable Autodetection Popup</p>
       <Switch
