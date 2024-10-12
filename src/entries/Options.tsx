fbafe7c9150c0b9b89c4dea85d5806b3d975d59a
josephreidy <161133156+josephreidy@users.noreply.github.com>
2024-10-12 12:18:42 +0100

    flexbox is back

--- src/entries/Options.tsx
+++ src/entries/Options.tsx
@@ -6,7 +6,7 @@ export function Options() {
   const [detectPopup, setDetectPopup] = createSignal(true);
 
   return (
-    <main>
+    <main class="flex flex-col items-center justify-center h-screen">
       <h1 class="text-2xl font-bold mb-12">Fine Print Ninja Options</h1>
       <p>Enable Autodetection Popup</p>
       <Switch
